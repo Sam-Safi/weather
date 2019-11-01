6afb3b4852eafbf3d21b133455eabb6da2549be3
sadegh <sadegh25_safikhani@yahoo.com>
2019-11-02 10:39:26 +1100

    debug

--- db/migrate/20191026061617_rename_info_to_city.rb
+++ db/migrate/20191026061617_rename_info_to_city.rb
@@ -3,6 +3,7 @@ class RenameInfoToCity < ActiveRecord::Migration[6.0]
     create_table :city do |t|
       t.string :city
       t.string :name
+      t.integer :user_id
       t.belongs_to :user
     end
   end
